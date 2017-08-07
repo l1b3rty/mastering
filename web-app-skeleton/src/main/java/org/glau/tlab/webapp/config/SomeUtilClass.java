a47f6781906f28c4c959bb289fd7c718055c7e0e
Igor Valashov <igor.valashov@gmail.com>
2017-08-08 00:25:06 +0300

    change util

--- web-app-skeleton/src/main/java/org/glau/tlab/webapp/config/SomeUtilClass.java
+++ web-app-skeleton/src/main/java/org/glau/tlab/webapp/config/SomeUtilClass.java
@@ -3,7 +3,7 @@ package org.glau.tlab.webapp.config;
 public class SomeUtilClass {
 
     public static void doNothing() {
-        String a = "test";
+        String a = "test123";
         System.out.println(a);
     }
 
